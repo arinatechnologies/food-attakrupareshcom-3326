6f8a0da4cb031af751cf6970a11c606d640fe9ac
arinatechnologies <155801643+arinatechnologies@users.noreply.github.com>
2025-06-30 07:55:58 -0500

    Upload from Lambda

---
+++ js/about.js
@@ -0,0 +1,10 @@
+{
+  "about_images": [
+    "food warehouse interior with stacked boxes",
+    "team of food industry professionals meeting",
+    "organic vegetables on farm",
+    "food quality inspection",
+    "food distribution truck",
+    "sustainable food packaging"
+  ]
+}
